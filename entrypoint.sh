089e89a4142fe06e35b22818d46e8b71178934ab
Richard van den Berg <richard@vdberg.org>
2021-11-03 12:20:46 +0100

    Use repo_path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-args="--regex --rules regexes.json --entropy=False"
+repo="."
+args="--regex --rules regexes.json --entropy=False --repo_path=${repo}"
 
 if [ -n "${INPUT_BRANCH}" ]; then
   args="${args} --branch ${INPUT_BRANCH}"
 fi
 
 cp /regexes.json .
-/usr/local/bin/trufflehog ${args} .
+/usr/local/bin/trufflehog ${args} ${repo}
