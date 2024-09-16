e93bc04c0af77e32283987475d3c1d8daeb92386
Darryn Ten <darryn.ten@gmail.com>
2024-09-16 15:35:37 +0700

    Debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,13 @@
 git config --global --add safe.directory '*'
 
 whoami
+pwd
+ls -lahtr .
 ls -lahtr /
 ls -lahtr /github
 ls -lahtr ~
 
-repo="."
-args="--regex --entropy=False --max_depth=50 --repo_path=${repo} --exclude_paths .truffleignore"
+args="--regex --entropy=False --max_depth=50 --exclude_paths .truffleignore"
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
@@ -23,5 +24,6 @@ cp /regexes.json .
 # By default the 'WORKDIR' of our Docker image is set to the 'GITHUB_WORKSPACE'
 # which is mounted into our image. This means, as long as a checkout action was
 # done before our action runs, we'll have access to the repository.
+repo="file://$(pwd)"
 query="$args $repo" # Build args query with repository url
 /usr/local/bin/trufflehog ${query}
